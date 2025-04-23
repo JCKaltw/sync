266551541a8b08eeeeb659335fd7d83c0ec5932d
JCKalt <ckaltwasser@reminingcorp.com>
2025-04-23 10:01:46 -0400

    init commit

---
+++ import-eyedro.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE_VAR="$(date +%Y%m%d)"
+EXPORT_DATA=export_data
+cd ${EXPORT_DATA}
+
+echo "Downloading from pg2..."
+scp pg2:sync/pg2-eyedro-pgdump-${DATE_VAR}.tgz pg2:sync/weather-db-${DATE_VAR}.tgz .
+tar xvf pg2-eyedro-pgdump-${DATE_VAR}.tgz
+
+echo "Importing eyedro (pgs2) database public schema..."
+psql -h ${PGHOST_2} -p ${PGPORT_2} -U ${PGUSER_2} -d ${PGDATABASE_2} -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" && psql -h ${PGHOST_2} -p ${PGPORT_2} -U ${PGUSER_2} -d ${PGDATABASE_2} -f public_schema_backup.sql
+
+echo "Importing eyedro (pgs2) database weather schema..."
+tar xvf weather-db-${DATE_VAR}.tgz
+psql -h ${PGHOST_2} -p ${PGPORT_2} -U ${PGUSER_2} -d ${PGDATABASE_2} -c "DROP SCHEMA weather CASCADE; CREATE SCHEMA weather;" && psql -h ${PGHOST_2} -p ${PGPORT_2} -U ${PGUSER_2} -d ${PGDATABASE_2} -f weather_schema_backup.sql
+
+cd ..
